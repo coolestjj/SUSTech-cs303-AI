d918ee664a539fcb258900d16298bedff5d285b2
cooljj <1009839598@qq.com>
2021-10-11 23:21:27 +0800

    First update. Passed 9 but failed 1 test case. Dead loop in line 93. Deal with it!!!

--- Reversi.py
+++ Reversi.py
@@ -10,6 +10,22 @@ random.seed(0)
 
 # don't change the class name
 class AI(object):
+    weight = np.array([
+        [70, -15, 20, 20, 20, 20, -15, 70],
+        [-20, -30, 5, 5, 5, 5, -30, -15],
+        [20, 5, 1, 1, 1, 1, 5, 20],
+        [20, 5, 1, 1, 1, 1, 5, 20],
+        [20, 5, 1, 1, 1, 1, 5, 20],
+        [20, 5, 1, 1, 1, 1, 5, 20],
+        [-20, -30, 5, 5, 5, 5, -30, -15],
+        [70, -15, 20, 20, 20, 20, -15, 70]
+    ])
+
+    weight = weight * (-1)
+
+    depth = 0
+    max_depth = 3
+
     # chessboard_size, color, time_out passed from agent
     def __init__(self, chessboard_size, color, time_out):
         self.chessboard_size = chessboard_size
@@ -26,11 +42,17 @@ class AI(object):
         # Clear candidate_list, must do this step
         self.candidate_list.clear()
         # ==================================================================
-        # Write your algorithm here
-        # Here is the simplest sample:Random decision
+        action_list = AI.get_all_actions(self, chessboard)
+        start = time.time()
+        action, weight = self.alpha_beta(chessboard, self.color, -9999999, 9999999, start)
+        self.candidate_list = action_list
+        self.candidate_list.remove(action)
+        self.candidate_list.append(action)
+
+    def get_all_actions(self, chessboard):
         idx = np.where(chessboard == self.color)
         idx = list(zip(idx[0], idx[1]))
-        # ==============Find new pos========================================
+        action_list = []
         drow = [-1, -1, -1, 0, 0, 1, 1, 1]
         dcol = [-1, 0, 1, -1, 1, -1, 0, 1]
         for my_piece in idx:
@@ -44,12 +66,61 @@ class AI(object):
                         col = col + dcol[i]
                         permission_to_go = True
                     elif chessboard[row][col] == 0 and permission_to_go is True:
-                        self.candidate_list.append((row, col))
+                        action_list.append((row, col))
                         break
                     else:
                         break
+        return action_list
 
+    def evaluate(self, chessboard):
+        count = 0
+        for i in range(self.chessboard_size):
+            for j in range(self.chessboard_size):
+                if chessboard[i][j] == self.color:
+                    count += self.weight[i][j]
+                elif chessboard[i][j] == -1 * self.color:
+                    count -= self.weight[i][j]
+        return count
 
+    def alpha_beta(self, chessboard, color, a, b, start):
+        if self.depth > self.max_depth:
+            return None, self.evaluate(chessboard)
+        action_list = AI.get_all_actions(self, chessboard)
+        if len(action_list) == 0:
+            run_time = time.time() - start
+            if run_time >= 2:
+                return None, self.evaluate(chessboard)
+            return self.alpha_beta(chessboard, -1 * color, a, b, start)
+
+        max = -9999999
+        min = 9999999
+        action = ()
+
+        for p in action_list:
+            self.depth += 1
+            p1, current = self.alpha_beta(chessboard, -1 * color, a, b, start)
+            self.depth -= 1
+            if color == self.color:
+                if current > a:
+                    if current > b:
+                        return p, current
+                    a = current
+                if current > max:
+                    max = current
+                    action = p
+            else:
+                if current < b:
+                    if current < a:
+                        return p, current
+                    b = current
+                if current < min:
+                    min = current
+                    action = p
+
+        if color == self.color:
+            return action, max
+        else:
+            return action, min
 
         # Make sure that the position of your decision in chess board is empty.
         # If not, the system will return error.
